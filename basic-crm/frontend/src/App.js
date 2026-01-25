e799dc144dcaf2389ab7122b976f2cb76f4367a3
ninoyerbas <ichbincesartobar@yahoo.com>
2026-01-24 19:02:05 -0800

    Create App.js for basic CRM frontend

---
+++ basic-crm/frontend/src/App.js
@@ -0,0 +1,15 @@
+// App component
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the CRM Frontend</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
